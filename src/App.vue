be0722e87116ef5e95b23973083611e2bb0e5e14
Damian <krupa5643@gmail.com>
2024-11-02 13:29:24 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-card>
+        <v-tabs
+          v-model="tab"
+          bg-color="primary"
+        >
+          <v-tab value="one">Item One</v-tab>
+          <v-tab value="two">Item Two</v-tab>
+          <v-tab value="three">Item Three</v-tab>
+        </v-tabs>
+
+        <v-card-text>
+          <v-tabs-window v-model="tab">
+            <v-tabs-window-item value="one">
+              One
+            </v-tabs-window-item>
+
+            <v-tabs-window-item value="two">
+              Two
+            </v-tabs-window-item>
+
+            <v-tabs-window-item value="three">
+              Three
+            </v-tabs-window-item>
+          </v-tabs-window>
+        </v-card-text>
+      </v-card>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+  const tab = ref('date')
+
+</script>
